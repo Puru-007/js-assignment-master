d4d06444aaaa833d476de6c8e54a1c2b1d0ca9c8
p0p04jh <purvesh.patil@walmart.com>
2024-05-29 18:36:02 +0530

    Match id, startTime & Format added for PS 2

--- src/controllers/sport.js
+++ src/controllers/sport.js
@@ -2,17 +2,22 @@ const Sport = require('../models/sport');
 
 const getAllSportsToursAndMatches = async () => {
     const matches = await Sport.getAllSportsToursAndMatches();
-    console.log("This are my matches - " + matches)
     const res = {};
     matches.forEach(match => {
-        const { id, sportName, tourName, matchName, startTime } = match;
+        const { sportName, tourName, matchName } = match;
         if (!res[sportName]) {
             res[sportName] = {};
         }
-        if (!res[sportName][tourName][id]) {
+        if (!res[sportName][tourName]) {
             res[sportName][tourName] = [];
         }
-        res[id][sportName][tourName].push(matchName);
+        // res[sportName][tourName].push(matchName);
+        res[sportName][tourName].push({
+                        id: match.id,
+                        startTime: match.startTime,
+                        format: match.format,
+                        matchName: match.matchName
+                    });
     });
     return res;
 }
--- src/models/sport.js
+++ src/models/sport.js
@@ -1,9 +1,6 @@
 const mysql = require('../lib/mysql');
 
 const getAllSportsToursAndMatches = async () => {
-    // const statement = 'select s.name as sportName, t.name as tourName, m.name as matchName ' +
-    //     'from matches m inner join tours t on m.tourId = t.id ' +
-    //     'inner join sports s on t.sportId = s.id';
     const statement = 'select m.id, m.startTime , m.format , s.name as sportName, t.name as tourName, m.name as matchName from matches m inner join tours t on m.tourId = t.id inner join sports s on t.sportId = s.id';
     const parameters = [];
     return await mysql.query(statement, parameters);
